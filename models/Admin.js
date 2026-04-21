1f5169f9e03d2ff3882b6ca75c2a26f76c9427b3
kalpeshparmar <parmarkalpesh1586@gmail.com>
2026-04-21 21:50:13 +0530

    implement

--- models/Admin.js
+++ models/Admin.js
@@ -10,7 +10,7 @@ const adminSchema = mongoose.Schema(
         email: {
             type: String,
             required: true,
-            unique: true,
+            unique: true, 
         },
         password: {
             type: String,
